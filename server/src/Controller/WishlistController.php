7cce5c6b7c577155958c64224c42c0dea5eecc0f
jeanphi-44 <jean-philippe.levesques@imt-atlantique.net>
2025-03-23 19:10:27 +0100

    update Controller+view

--- server/src/Controller/WishlistController.php
+++ server/src/Controller/WishlistController.php
@@ -1,37 +1,45 @@
 <?php
 
+
 namespace App\Controller;
 
+use App\Repository\ItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class WishlistController extends AbstractController
-{
-    #[Route('/wishlist/123456', name: 'view_wishlist')]
-    // public function viewWishlist($id, WishlistItemRepository $wishlistItemRepository)
-    // {
-    //    // Récupérer les items de la wishlist depuis la base de données
-    //    $items = $wishlistItemRepository->findBy(['wishlist' => $id]);
 
-    //    return $this->render('wishlist/view.html.twig', [
-    //        'items' => $items
-    //    ]);
-    //  }
-    public function viewWishlist() : Response
+{
+    #[Route('/wishlist/{id}', name: 'view_wishlist')]
+    public function viewWishlist($id, ItemRepository $itemRepository) : Response
     {
-       // Simuler des données pour tester
-       $wishlist = [
-           'id' => 1,
-           'title' => 'Wishlist de Test',
-           'items' => [
-               ['name' => 'Livre Symfony', 'price' => 29.99, 'purchaseUrl' => 'https://exemple.com/livre', 'purchasedBy' => 'Julie', 'description' => '', 'congratulatoryMessage' => 'test'],
-               ['name' => 'Casque Audio', 'price' => 99.99, 'purchaseUrl' => 'https://exemple.com/casque', 'purchasedBy' => 'JP', 'description' => 'test', 'congratulatoryMessage' => 'test']
-           ]
-       ];
-   
+       // On récupère les items de la wishlist depuis la base de données
+       $items = $itemRepository->findBy(['wishlist' => $id]);
+
        return $this->render('wishlist/view.html.twig', [
-           'wishlist' => $wishlist
+           'wishlist' => [
+            'id'=> $id, 
+            'title' => 'Wishlist de Rico', 
+            'items' => $items
+           ]
        ]);
-   }
+     }
 }
+?> 
+//     public function viewWishlist() : Response
+//     {
+//        // Simuler des données pour tester
+//        $wishlist = [
+//            'id' => 1,
+//            'title' => 'Wishlist de Test',
+//            'items' => [
+//                ['name' => 'Livre Symfony', 'price' => 29.99, 'purchaseUrl' => 'https://exemple.com/livre', 'purchasedBy' => 'Julie', 'description' => 'Ceci un livre enrichissant', 'congratulatoryMessage' => 'Bravo!!'],
+//                ['name' => 'Casque Audio', 'price' => 99.99, 'purchaseUrl' => 'https://exemple.com/casque', 'purchasedBy' => '', 'description' => 'Super casque', 'congratulatoryMessage' => '']
+//            ]
+//        ];
+   
+//        return $this->render('wishlist/view.html.twig', [
+//            'wishlist' => $wishlist
+//        ]);
+//    }
