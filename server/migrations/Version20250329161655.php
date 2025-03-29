78e6f17e5d748640929113a011ddcc33312ef16a
Tchadel Icard <tchadelicard@protonmail.com>
2025-03-29 17:18:08 +0100

    fix: remove all migrations and create a new one

--- server/migrations/Version20250329161655.php
+++ server/migrations/Version20250329161655.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250325161055 extends AbstractMigration
+final class Version20250329161655 extends AbstractMigration
 {
     public function getDescription(): string
     {
